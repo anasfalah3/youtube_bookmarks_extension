81ec460d005a33be75304a6725125c749a6c3fd7
anasfalah3 <anasfalah3@gmail.com>
2024-07-27 22:33:36 +0100

    extension logic

---
+++ contentScript.js
@@ -0,0 +1,16 @@
+(()=>{
+      let youtubeLeftControls, youtubePlayer;
+      let currentVideo =""
+      chrome.runtime.onMessage.addListener((obj,sender,response)=>{
+            const {type,value,videoId} = obj
+            
+            if (type ==+ "NEW") {
+                  currentVideo = videoId
+                  newVideoLoaded()
+            }
+      })
+      const newVideoLoaded = ()=>{
+            const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0]
+            console.log(bookmarkBtnExists);
+      }
+})()
